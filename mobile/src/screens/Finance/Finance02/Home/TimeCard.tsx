b31bb7262f3dfa3c2a86e2bfa5a651b199b8dd61
Stalamek <nicolas.ximinic@codice.dev>
2024-02-08 12:24:17 -0300

    card for homepage, needs changes

---
+++ mobile/src/screens/Finance/Finance02/Home/TimeCard.tsx
@@ -0,0 +1,47 @@
+import { StyleService, useStyleSheet } from '@ui-kitten/components'
+import { LayoutCustom, Text } from 'components'
+import {Image, ImageBackground} from 'react-native';
+import React from 'react'
+import theme from 'theme';
+
+interface CreditCardProps {
+    color: string;
+    balance: number;
+    card_number: string;
+    icon: string
+  }
+
+const TimeCard = ({ item }: { item: CreditCardProps }) => {
+  const styles = useStyleSheet(themedStyles);
+  return (
+    <LayoutCustom style={{
+        ...styles.card
+      }}>
+        <ImageBackground style={{height:'100%',backgroundColor: item.color,borderRadius: 10}} source={require("../../../../assets/images/icons/colorful-background.png")}>
+        <LayoutCustom alignSelfCenter itemsCenter padding={theme.paddings.medium} style={{}}>
+        <Image source={item.icon}/>
+        <Text>hola</Text>
+        <Text>$1.323.434,00</Text>
+        </LayoutCustom>
+        </ImageBackground>
+    </LayoutCustom>
+  )
+}
+
+export default TimeCard
+
+const themedStyles = StyleService.create({
+    card: {
+      borderRadius: 16,
+      marginVertical: 16,
+      shadowOffset: {
+        width: 8,
+        height: 8,
+      },
+      shadowOpacity: 0.4,
+      shadowRadius: 8,
+      elevation: 10,
+      marginRight:theme.margins.small,
+      height: '85%'
+    },
+  });
