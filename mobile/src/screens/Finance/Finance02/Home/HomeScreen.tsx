3c830a6ab843f322bb41d4db340b555f859326ef
Stalamek <nicolas.ximinic@codice.dev>
2024-02-08 14:05:46 -0300

    changes in screen, needs fixes in cards

--- mobile/src/screens/Finance/Finance02/Home/HomeScreen.tsx
+++ mobile/src/screens/Finance/Finance02/Home/HomeScreen.tsx
@@ -33,6 +33,8 @@ import IButton from "./IButton";
 import EvaIcons from "types/eva-icon-enum";
 import { Icons } from "assets/icons";
 import ActivityItem from "./ActivityItem";
+import TimeCard from "./TimeCard";
+import theme from "theme";
 
 const HomeScreen = React.memo(() => {
   const theme = useTheme();
@@ -53,11 +55,11 @@ const HomeScreen = React.memo(() => {
             onPress={() => navigate("FinanceIntro")}
           />
         )}
-        accessoryRight={() => <RoundedButton icon="bell" />}
+        accessoryRight={() => <RoundedButton icon="person_fill" />}
       />
       <Content contentContainerStyle={styles.content}>
         <LayoutCustom itemsCenter mt={10} mb={10}>
-        <Text style={{fontSize:20}}>Disponibilidad</Text>
+        <Text fontSize={20} marginBottom={10} marginTop={10}>Disponibilidad</Text>
         </LayoutCustom>
         <Carousel
           data={CARDS}
@@ -79,10 +81,11 @@ const HomeScreen = React.memo(() => {
             parallaxScrollingOffset: 10,
           }}
           renderItem={({ item, index }) => {
-            return <CreditCard item={item} key={index} />;
+            return  <TimeCard item={item} />
+            //  <CreditCard item={item} key={index} />;
           }}
         />
-        <LayoutCustom horizontal itemsCenter justify="center" gap={8}>
+        {/* <LayoutCustom horizontal itemsCenter justify="center" gap={8}>
           {CARDS.map((item, index) => {
             return (
               <PaginationItem
@@ -94,25 +97,24 @@ const HomeScreen = React.memo(() => {
               />
             );
           })}
-        </LayoutCustom>
+        </LayoutCustom> */}
         <LayoutCustom horizontal itemsCenter justify="center"  mt={20}>
-          <Image style={{ width: 65, height: 65, }} source={require("../../../../assets/images/icons/moneyStat.png")} />
-          <LayoutCustom ml={20}>
-            <Text style={{ fontSize: 20, marginBottom: 7 }}>Posiciones</Text>
-            <Text style={{ fontSize: 20 }}>$2.455.342,88</Text>
+          <Image style={{ width: 70, height: 70, }} source={require("../../../../assets/images/icons/moneyStat.png")} />
+          <LayoutCustom ml={20} style={{alignItems:"flex-start"}}>
+            <Text style={{ fontSize: 20, marginBottom: 3 }}>Posiciones</Text>
+            <Text style={themedStyles.moneyText} fontSize={20} category="t5">$20.455.342,88</Text>
           </LayoutCustom>
         </LayoutCustom>
         <LayoutCustom
           horizontal
           itemsCenter
           justify="center"
-          gap={48}
-          mt={32}
-          mb={40}
+          gap={30}
+          mt={50}
+          mb={35}
         >
-          <IButton icon="wallet_send" title="Informar orden" />
-          {/* <IButton icon="qr" title="QR Actions" /> */}
-          <IButton icon="document" title={`Solicitar\ntransferencia`} />
+          <IButton icon="wallet_send" title={`Informar\norden`} />
+          <IButton icon="document" title={`Solicitar transferencia`} />
         </LayoutCustom>
         {/* <LayoutCustom horizontal itemsCenter justify="space-between" mh={24}>
           <Text>Recent Activities</Text>
@@ -141,21 +143,23 @@ export default HomeScreen;
 
 const themedStyles = StyleService.create({
   container: {
-    flex: 1,
-    paddingBottom: 0,
+    flex: 1
   },
   content: {
-    flexGrow: 1,
+    // flexGrow: 1,
   },
   topNavigation: {
     paddingHorizontal: 24,
   },
+  moneyText: {
+    color: theme.colors.skyBlue
+  }
 });
 
 const CARDS = [
-  { color: "#8438FF", balance: 15245.9, card_number: "5282300014453286" },
-  { color: "#38CFFF", balance: 24245.9, card_number: "5282300014453286" },
-  { color: "#FF7438", balance: 151245.9, card_number: "5282300014453286" },
+  { color: "#009F9F", balance: 15245.9, card_number: "5282300014453286", icon: require('../../../../assets/images/icons/today-clock.png') },
+  { color: "#D0682E", balance: 24245.9, card_number: "5282300014453286", icon: require('../../../../assets/images/icons/today-clock.png') },
+  { color: "#701BC4", balance: 151245.9, card_number: "5282300014453286", icon: require('../../../../assets/images/icons/today-clock.png') },
 ];
 
 const Activities = [
