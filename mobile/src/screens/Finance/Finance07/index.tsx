2f4b465c0e4ad7e5ee385f493d13d788c2a52f4b
Stalamek <nicolas.ximinic@codice.dev>
2024-02-19 10:34:46 -0300

    added modal logic

--- mobile/src/screens/Finance/Finance07/index.tsx
+++ mobile/src/screens/Finance/Finance07/index.tsx
@@ -16,15 +16,17 @@ import {
   Container,
   Content,
   LayoutCustom,
-  RoundedButton
+  RoundedButton,
+  Text
 } from "components";
-import TransactionItem from "./TransactionItem";
+import TransactionItem, { ITransactionItemProps } from "./TransactionItem";
 // ----------------------------- Types ----------------------------------------
 // ----------------------------- Reanimated 2 -----------------------
 import { useSharedValue } from "react-native-reanimated";
 // ----------------------------- Navigation -----------------------
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import CurrencyToggle from "components/Switch";
+import { Modal, Pressable } from "react-native";
 import theme from "theme";
 import { FinanceStackParamList } from "types/navigation-types";
 import BalanceCard from "./BalanceCard";
@@ -34,6 +36,8 @@ const Finance07 = React.memo(() => {
   const styles = useStyleSheet(themedStyles);
   const { width } = useLayout();
   const time_now = new Date().getHours();
+  const [selectedTransaction, setSelectedTransaction] = useState<ITransactionItemProps | {}>({})
+  const [open, setOpen] = useState(false)
   const [currency, setCurrency] = useState('ARS')
   const _intro = () => {
     if (time_now >= 0 && time_now < 12) {
@@ -50,36 +54,68 @@ const Finance07 = React.memo(() => {
 
   const { navigate } = useNavigation<NavigationProp<FinanceStackParamList>>();
 
+  const selectTransaction = (data: ITransactionItemProps) => {
+    setSelectedTransaction(data)
+    setOpen(true)
+  }
+
   const progressValue = useSharedValue(0);
 
   return (
-    <Container style={styles.container}>
-      <TopNavigation
-        alignment="center"
-        title="Disponibilidad"
-        style={styles.topNavigation}
-        accessoryLeft={() => (
-          <RoundedButton
-            icon="arrow-left"
-            onPress={() => navigate("FinanceIntro")}
-          />
-        )}
-        accessoryRight={() => <RoundedButton icon="bell" />}
-      />
-      <Content contentContainerStyle={styles.content}>
-        <LayoutCustom mt={theme.margins.large} mb={theme.margins.small}>
-          <LayoutCustom alignSelfCenter mb={theme.margins.medium}>
-            <CurrencyToggle changeCurrency={setCurrency} />
+    <>
+      <Modal
+        animationType="fade"
+        visible={open}
+        transparent={true}
+        onRequestClose={() => setOpen(false)}
+      >
+        <LayoutCustom style={styles.centeredView}>
+          <LayoutCustom style={styles.modalView}>
+            <LayoutCustom mb={theme.margins.large}>
+              <Text marginBottom={theme.margins.medium} fontSize={20} style={styles.modalText}>Detalle del movimiento</Text>
+              <Text category="subhead" marginBottom={theme.margins.xSmall} fontSize={16} style={styles.modalText}> Fecha:</Text>
+              <Text marginBottom={theme.margins.xSmall} fontSize={18} style={styles.modalText}>11/01/23</Text>
+              <Text category="subhead" marginBottom={theme.margins.xSmall} fontSize={16} style={styles.modalText}>Importe:</Text>
+              <Text marginBottom={theme.margins.xSmall} fontSize={18} style={styles.modalText}>$30000</Text>
+              <Text marginBottom={theme.margins.xSmall} style={styles.modalText}>CAUCION TOMADORA</Text>
+            </LayoutCustom>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setOpen(false)}
+            >
+              <Text style={styles.textStyle}>Volver</Text>
+            </Pressable>
           </LayoutCustom>
-          <BalanceCard balance={233004.91} grow={12.2} />
         </LayoutCustom>
-        <LayoutCustom overflow="scroll" gap={15} mh={theme.margins.medium}>
-          {SAMPLE_TRANSACTION.map((transaction, index) => {
-            return <TransactionItem data={transaction} key={index} />;
-          })}
-        </LayoutCustom>
-      </Content>
-    </Container>
+      </Modal>
+      <Container style={styles.container}>
+        <TopNavigation
+          alignment="center"
+          title="Disponibilidad"
+          style={styles.topNavigation}
+          accessoryLeft={() => (
+            <RoundedButton
+              icon="arrow-left"
+              onPress={() => navigate("FinanceIntro")}
+            />
+          )}
+          accessoryRight={() => <RoundedButton icon="bell" />}
+        />
+        <Content contentContainerStyle={styles.content}>
+          <LayoutCustom mt={theme.margins.large} mb={theme.margins.small}>
+            <LayoutCustom alignSelfCenter mb={theme.margins.medium}>
+              <CurrencyToggle changeCurrency={setCurrency} />
+            </LayoutCustom>
+            <BalanceCard balance={233004.91} grow={12.2} />
+          </LayoutCustom>
+          <LayoutCustom overflow="scroll" gap={15} mh={theme.margins.medium}>
+            {SAMPLE_TRANSACTION.map((transaction, index) => {
+              return <TransactionItem data={transaction} key={index} selectTransaction={selectTransaction} />;
+            })}
+          </LayoutCustom>
+        </Content>
+      </Container>
+    </>
   );
 });
 
@@ -95,6 +131,44 @@ const themedStyles = StyleService.create({
   },
   content: {
     overflow: 'scroll'
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 25,
+    padding: 15,
+    elevation: 2,
+    paddingHorizontal: theme.paddings.large
+  },
+  buttonClose: {
+    backgroundColor: '#009F9F',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    textAlign: 'center',
   },
 });
 
