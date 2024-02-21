b9b380c063ef5989caba832df9bc46fcb9234fc2
Matías Trovant <mtrovant@gmail.com>
2024-02-21 15:03:25 -0300

    Cambios

--- backend/src/rosval-http/rosval-http.service.ts
+++ backend/src/rosval-http/rosval-http.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 
-
 @Injectable()
 export class RosvalHttpService {
-  constructor(){
-    if(!RosvalHttpService.http) {
-      RosvalHttpService.http = axios.create({baseURL: 'https://agentes.rosval.com.ar/Irmo/api/'})
-    RosvalHttpService.http.interceptors.response.use(null, this.handleErrorInterceptor)
+  constructor() {
+    if (!RosvalHttpService.http) {
+      RosvalHttpService.http = axios.create({
+        baseURL: 'https://agentes.rosval.com.ar/Irmo/api/',
+      });
+      RosvalHttpService.http.interceptors.response.use(
+        null,
+        this.handleErrorInterceptor,
+      );
+    }
+    this.http = RosvalHttpService.http;
   }
-  this.http = RosvalHttpService.http;
-}
-  
-private static http: AxiosInstance 
-http: AxiosInstance
 
-tokenRosval: string | undefined;
+  private static http: AxiosInstance;
+  http: AxiosInstance;
 
- handleErrorInterceptor  = async (error: any) => {
+  tokenRosval: string | undefined;
+
+  handleErrorInterceptor = async (error: any) => {
     const originalRequest = error.config;
     //Si el status no es de no authorizado (401) el handler no se ocupa del error
     if (error.response?.status !== 401) throw error;
@@ -34,25 +38,25 @@ tokenRosval: string | undefined;
     //Se reintenta la request
     return this.http(newRequest);
   };
-  
+
   /**
    * Asigna cabecera de authenticación a una `requestConfig` de `axios`. Toma el accesToken del `asyncStorage`
    * @param {*} config
    * @returns
    */
   async setAuthHeaderToConfig(config: InternalAxiosRequestConfig<any>) {
-    this.http.defaults.headers.common['Authorization'] =`Bearer ${this.tokenRosval}`
+    this.http.defaults.headers.common['Authorization'] =
+      `Bearer ${this.tokenRosval}`;
     return config;
   }
-  
+
   async getBearerToken(): Promise<string> {
     const response = await this.http.post(`login`, {
       clientId: '423000005',
       username: 'API_Resumen',
       password: 'API_Resumen',
     });
-    
+
     return response.data.token;
   }
-  
 }
