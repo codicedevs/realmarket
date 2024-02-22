967a4b0918034ded0a744f05d448fe3e41eecb57
LucasMarelli <lucas.marelli@codice.dev>
2024-02-22 13:13:18 -0300

    Fix

--- backend/src/movimientos/movimientos.service.ts
+++ backend/src/movimientos/movimientos.service.ts
@@ -5,7 +5,7 @@ import { Movimiento } from './entities/movimiento.entity';
 @Injectable()
 export class MovimientosService extends RosvalHttpService {
   async findByDate(from: string, to: string): Promise<Movimiento[]> {
-    const response = await this.http.get<Movimiento[]>(
+    const response = await this.get<Movimiento[]>(
       `cuentas/423000005/movimientos?fechaDesde=${from}&fechaHasta=${to}`,
     );
     return response.data;
