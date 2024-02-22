1cced8c4f198d4695f25582b95e15db6e0f8aaf7
Matías Trovant <mtrovant@gmail.com>
2024-02-22 12:06:44 -0300

    Reemplazo Servicio HTTP por paquete Codice

--- backend/src/posiciones/posiciones.service.ts
+++ backend/src/posiciones/posiciones.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 
 import { RosvalHttpService } from 'src/rosval-http/rosval-http.service';
 import { Posicion } from 'src/types/posicion';
@@ -8,10 +8,15 @@ import { UpdatePosicioneDto } from './dto/update-posicione.dto';
 @Injectable()
 export class PosicionesService extends RosvalHttpService {
   async findByDate(accountId: string, from: string): Promise<Posicion[]> {
-    const response = await this.http.get<Posicion[]>(
-      `cuentas/${accountId}/posiciones?fecha=${from}&tipoTitulo=Acciones`,
-    );
-    return response.data;
+    try {
+      const response = await this.get<Posicion[]>(
+        `cuentas/${accountId}/posiciones?fecha=${from}&tipoTitulo=Acciones`,
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw new HttpException('Invalid response', HttpStatus.CONFLICT);
+    }
   }
 
   create(createPosicioneDto: CreatePosicioneDto) {
