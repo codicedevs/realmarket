e81db6f01a711c6333a60982913b8148c77145fc
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-03-06 11:45:24 -0300

    posiciones

--- backend/src/posiciones/posiciones.service.ts
+++ backend/src/posiciones/posiciones.service.ts
@@ -16,10 +16,10 @@ export class PosicionesService extends RosvalHttpService {
 
   async getCashPositionByDates(accountId: string) {
     /**Esta pantalla muestra el SALDO en Efvo PESOS de la cuenta HOY/24Hs/48Hs */
-    // const posicionesHoy = await this.findByDate(
-    //   accountId,
-    //   formatRosvalDate(dayjs()),
-    // );
+    const posicionesHoy = await this.findByDate(
+      accountId,
+      formatRosvalDate(dayjs()),
+    );
     const posiciones24 = await this.findByDate(
       accountId,
       formatRosvalDate(dayjs().add(1, 'day')),
@@ -28,11 +28,11 @@ export class PosicionesService extends RosvalHttpService {
     //   accountId,
     //   formatRosvalDate(dayjs().add(2, 'day')),
     // );
-    // const dispoHoy = posicionesHoy.find((e) => e.tipoTitulo === 'Moneda');
+    const dispoHoy = posicionesHoy.find((e) => e.tipoTitulo === 'Moneda');
     const dispo24 = posiciones24.find((e) => e.tipoTitulo === 'Moneda');
     // const dispo48 = posiciones48.find((e) => e.tipoTitulo === 'Moneda');
     return {
-      // dispoHoy: dispoHoy.cantidadLiquidada * -1,
+      dispoHoy: dispoHoy.cantidadLiquidada * -1,
       dispo24: dispo24.cantidadLiquidada * -1,
       // dispo48: dispo48.cantidadLiquidada * -1,
     };
