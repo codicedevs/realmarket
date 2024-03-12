f0a9134e3bb600381b9fe3aa493869bd84b64300
Matías Trovant <mtrovant@gmail.com>
2024-03-12 15:41:40 -0300

    Change Bearer BCRA

--- backend/src/utils/dolar.ts
+++ backend/src/utils/dolar.ts
@@ -1,17 +1,20 @@
 import axios from 'axios';
 
-const bearer =
+const bearerBcra =
   'BEARER ' +
   'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDE3OTkzMjMsInR5cGUiOiJleHRlcm5hbCIsInVzZXIiOiJtdHJvdmFudEBnbWFpbC5jb20ifQ.LAJPY8vNUSNPJ0SfzC4TrSfg5oIjZO1E8KQ0kfq8wPkcFng-PqqFOMoGaXN1SkPt4K64nELxp1-NcfBHstmXdg';
 
+// Consultado a https://dolarapi.com/docs/operations/get-dolar-oficial.html  -- API GRATUITA y DIARIA
 export const getDolar = async () => {
   const response = await axios.get('https://dolarapi.com/v1/dolares/oficial');
   return response.data;
 };
 
+// Consultado a https://estadisticasbcra.com/api/documentacion -- TIENE 100 CONSULTAS DIARIAS LIBRES y el BEARER TOKEN VENCE AL AÑO
+
 export const getDolarBcra = async () => {
   const response = await axios.get('https://api.estadisticasbcra.com/usd_of', {
-    headers: { Authorization: bearer },
+    headers: { Authorization: bearerBcra },
   });
   return response.data[response.data.length - 1];
 };
