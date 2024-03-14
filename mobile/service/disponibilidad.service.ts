7c5b5aafb1688c18ae6a998e75d03814cbe39bda
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-03-14 11:40:19 -0300

    added service

--- mobile/service/disponibilidad.service.ts
+++ mobile/service/disponibilidad.service.ts
@@ -9,6 +9,10 @@ export class DisponbilidadService extends HttpService {
    async getCashPositions ()  {
     return this.get(`/cash-position-by-dates`)
    }
+
+   async totalPositions() {
+    return this.get(`/total-position`)
+   }
 }
 
 export default new DisponbilidadService()
