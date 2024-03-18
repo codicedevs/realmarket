cc2f33306e55d573ad7152ab833b5afbe518186a
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-03-18 12:57:06 -0300

    formatea el signo "-" en el numero

--- mobile/utils/number.ts
+++ mobile/utils/number.ts
@@ -1,9 +1,30 @@
 
+// export function currencyFormat(number: number, currency: string): string {
+//   const newNumber = number * 1
+//   return new Intl.NumberFormat('es-AR', {
+//     style: 'currency',
+//     currency: currency,
+//     minimumFractionDigits: 2,
+//     maximumFractionDigits: 2
+//   }).format(newNumber);
+// }
+
 export function currencyFormat(number: number, currency: string): string {
-  return new Intl.NumberFormat('es-AR', {
+  const isNegative = number < 0;
+  const absoluteNumber = Math.abs(number);
+
+  let formattedNumber = new Intl.NumberFormat('es-AR', {
     style: 'currency',
     currency: currency,
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
-  }).format(number);
+  }).format(absoluteNumber);
+
+  if (isNegative) {
+    const currencySymbol = formattedNumber.match(/^\D+/)[0];
+    const numericPart = formattedNumber.slice(currencySymbol.length);
+    formattedNumber = `${currencySymbol}- ${numericPart}`;
+  }
+
+  return formattedNumber;
 }
