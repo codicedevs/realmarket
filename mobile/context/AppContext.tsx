96d88280f6e4465c31a3c38963094669c4d8b7c5
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-03-20 11:48:48 -0300

    added app context

---
+++ mobile/context/AppContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, FC, ReactNode, useState } from "react";
+
+interface AppContextProps {
+  currency: string;
+  setCurrency: (currency: string) => void;
+}
+
+const initialContext: AppContextProps = {
+  currency: null,
+  setCurrency: () => 'ARS',
+};
+
+export const AppContext = createContext<AppContextProps>(initialContext);
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+const AppProvider: FC<AppProviderProps> = ({ children }) => {
+  const [currency, setCurrency] = useState<string>('ARS')
+  // const findUser = () => {
+  //     try {
+  //         const user = localStorage.getItem("currentUser")
+  //         if (!user) return
+  //         setCurrentUser(JSON.parse(user))
+  //     } catch (err) {
+  //         console.error(err)
+  //     } finally {
+  //         setTimeout(() => {
+  //             setLoading(false)
+  //         }, 2000)
+  //     }
+  // 
+  // useEffect(() => {
+  //     findUser()
+  // }, [])
+
+  return (
+    <AppContext.Provider
+      value={{
+        currency,
+        setCurrency,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppProvider;
