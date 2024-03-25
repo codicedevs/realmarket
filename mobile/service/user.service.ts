dbee73ecb75d014754837c243e73082bde588bef
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-03-25 16:58:47 -0300

    added userservice

---
+++ mobile/service/user.service.ts
@@ -0,0 +1,19 @@
+// import HttpService from "./http.service";
+import { HttpService } from "./http.service";
+
+export class UserService extends HttpService {
+  constructor() {
+    super("users")
+  }
+
+  async ChangePassword({ currentPass, newPass }: { currentPass: string, newPass: string }) {
+    console.log(currentPass)
+    console.log(newPass)
+    return this.post(`/changePass`, {
+      currentPast: currentPass,
+      newPass: newPass
+    })
+  }
+}
+
+export default new UserService()
