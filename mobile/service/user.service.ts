464dd5ce65bff1853b85996edb65f09de65df654
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-03-26 11:01:15 -0300

    fix

--- mobile/service/user.service.ts
+++ mobile/service/user.service.ts
@@ -7,8 +7,6 @@ export class UserService extends HttpService {
   }
 
   async ChangePassword({ currentPass, newPass }: { currentPass: string, newPass: string }) {
-    console.log(currentPass)
-    console.log(newPass)
     return this.post(`/changePass`, {
       currentPass: currentPass,
       newPass: newPass
