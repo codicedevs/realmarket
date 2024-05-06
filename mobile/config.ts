ae267a48887ef14151d9b213c83bbd52510153c7
Galia <tanguito@gmail.com>
2024-05-06 13:37:53 -0300

    Reemplazado hardcoding de config.ts por env vars

--- mobile/config.ts
+++ mobile/config.ts
@@ -1,2 +1,4 @@
-// export const BASE_URL = "http://192.168.1.27:8000"
-export const BASE_URL = "https://codice.dev:3011"
+// Set EXPO_PUBLIC_BASE_URL in your .env file (ignored in git):
+// - to connect to your local backend, use your Local network IP (i.e. http://192.168.0.126:8000)
+// - to connect to the development backend, use https://codice.dev:3011
+export const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL
