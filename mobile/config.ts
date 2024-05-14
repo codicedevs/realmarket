77b2310a9d1086ffc82eb0e9e7e2047cfa005b6b
Matías Trovant <mtrovant@gmail.com>
2024-05-14 13:43:42 -0300

    Cambios para que android Funcione (librerias) y cambio de signo en endPoints para getMovs

--- mobile/config.ts
+++ mobile/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3011"
-// export const BASE_URL = "https://codice.dev:3011"
+// export const BASE_URL = "http://localhost:3011"
+export const BASE_URL = "https://codice.dev:3011";
