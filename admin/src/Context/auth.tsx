3d3b8b6b2ddca13583327a73bd95a2562015c7e0
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-06-04 13:51:14 -0300

    introduce splash

--- admin/src/Context/auth.tsx
+++ admin/src/Context/auth.tsx
@@ -1,40 +1,54 @@
 import * as React from 'react';
+import Splash from '../components/splash/SplashLogo';
 import authService from '../service/auth.service';
 import { IUser, UserInfo } from '../types/user.type';
 
+//Tipo del contexto
 type AuthContextType = {
     user: IUser | null;
     login: (data: UserInfo) => Promise<void>;
     logout: () => Promise<void>;
     checkSession: () => Promise<void>;
 };
-
+//genero el contexto
 const AuthContext = React.createContext<AuthContextType | undefined>(undefined);
-
+//tipo de funcion
 type AuthProviderProps = {
     children: React.ReactNode;
 };
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
     const [user, setUser] = React.useState<IUser | null>(null);
+    const [loading, setLoading] = React.useState(false)
 
+    //funcion del login
     const login = async (data: UserInfo) => {
         const userInfo = await authService.login(data.email, data.password)
         if (userInfo) setUser(userInfo.user);
     };
 
+    //funcion del logout, falta cambiar su logica
     const logout = async () => {
         setUser(null);
     };
 
+    //chequea si ya hay una sesion iniciada
     const checkSession = async () => {
+        setLoading(true)
         const res = await authService.whoami()
         setUser(res.data)
+        setTimeout(() => {
+            setLoading(false)
+        }, 1000);
     }
 
     return (
         <AuthContext.Provider value={{ user, login, logout, checkSession }}>
-            {children}
+            {
+                loading ?
+                    <Splash />
+                    :
+                    children}
         </AuthContext.Provider>
     );
 };
