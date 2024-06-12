c6924a6664348ed92c044ae862117c6956c1ee7f
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-06-12 11:53:17 -0300

    use component

--- mobile/app/(finance)/home/index.tsx
+++ mobile/app/(finance)/home/index.tsx
@@ -11,6 +11,7 @@ import Container from '../../../components/Container'
 import CurrencyToggle from '../../../components/CurrencyToggle'
 import Header from '../../../components/CustomHeader'
 import LayoutCustom from '../../../components/LayoutCustom'
+import LoaderModal from '../../../components/LoaderModal'
 import TimeCard from '../../../components/cards/TimeCard'
 import OrderModal from '../../../components/orderModal'
 import { AppContext } from '../../../context/AppContext'
@@ -103,72 +104,74 @@ const Home = () => {
 
   useEffect(() => {
     getCash()
-
   }, [])
 
   const progressValue = useSharedValue<number>(0);
   return (
-    <Container style={{ backgroundColor: theme.colors.background }}>
-      {order && <OrderModal order={order} onClose={() => setOrder(null)} />}
-      <LayoutCustom>
-        <Header title={`Hola, ${typeof session === 'object' && session.nombre}`} />
-        <LayoutCustom itemsCenter mt={theme.margins.large} mb={theme.margins.medium}>
-          <CurrencyToggle />
-        </LayoutCustom>
-        <LayoutCustom horizontal ph={theme.paddings.medium}>
-          {
-            isLoading ?
-              <ActivityIndicator size={'small'} />
-              :
-              null
-          }
-          <Text style={themedStyles.lastUpdateText}>Última actualización: {!isLoading ? new Date().toLocaleDateString() : null}</Text>
-        </LayoutCustom>
-        <Carousel
-          data={CARDS}
-          width={windowWidth * 0.9}
-          height={212 * (windowHeight / 812)}
-          loop={false}
-          style={{ ...themedStyles.carouselStyle, width: windowWidth }}
-          onProgressChange={(_, absoluteProgress) =>
-            (progressValue.value = absoluteProgress)
-          }
-          mode="parallax"
-          modeConfig={{
-            parallaxScrollingScale: 0.96,
-            parallaxScrollingOffset: 10,
-          }}
-          renderItem={({ item, index }) => {
-            return (
-              <Touchable activeOpacity={1} key={index} style={{ height: '100%' }} onPress={() => router.navigate('/home/disponibilidad')}>
-                <TimeCard item={item} currency={currency} />
-              </Touchable>
-            )
-          }}
-        />
-        <TouchableOpacity activeOpacity={1} onPress={positionRoute}>
-          <LayoutCustom horizontal itemsCenter justify='flex-start' mv={theme.margins.medium} pl={theme.paddings.large}>
-            <Image style={themedStyles.img} source={require("../../../assets/Icons/moneyStat.png")} />
-            <LayoutCustom ml={theme.margins.small} style={{ alignItems: "flex-start" }}>
-              <Text style={themedStyles.position}>Posiciones</Text>
-              <Text style={themedStyles.moneyText}>{isLoading ? <ActivityIndicator size={'small'} /> : currencyFormat(currency === "ARS" ? positions.arsPositions : positions.usdPositions, currency)}</Text>
+    <>
+      <LoaderModal />
+      <Container style={{ backgroundColor: theme.colors.background }}>
+        {order && <OrderModal order={order} onClose={() => setOrder(null)} />}
+        <LayoutCustom>
+          <Header title={`Hola, ${typeof session === 'object' && session.nombre}`} />
+          <LayoutCustom itemsCenter mt={theme.margins.large} mb={theme.margins.medium}>
+            <CurrencyToggle />
+          </LayoutCustom>
+          <LayoutCustom horizontal ph={theme.paddings.medium}>
+            {
+              isLoading ?
+                <ActivityIndicator size={'small'} />
+                :
+                null
+            }
+            <Text style={themedStyles.lastUpdateText}>Última actualización: {!isLoading ? new Date().toLocaleDateString() : null}</Text>
+          </LayoutCustom>
+          <Carousel
+            data={CARDS}
+            width={windowWidth * 0.9}
+            height={212 * (windowHeight / 812)}
+            loop={false}
+            style={{ ...themedStyles.carouselStyle, width: windowWidth }}
+            onProgressChange={(_, absoluteProgress) =>
+              (progressValue.value = absoluteProgress)
+            }
+            mode="parallax"
+            modeConfig={{
+              parallaxScrollingScale: 0.96,
+              parallaxScrollingOffset: 10,
+            }}
+            renderItem={({ item, index }) => {
+              return (
+                <Touchable activeOpacity={1} key={index} style={{ height: '100%' }} onPress={() => router.navigate('/home/disponibilidad')}>
+                  <TimeCard item={item} currency={currency} />
+                </Touchable>
+              )
+            }}
+          />
+          <TouchableOpacity activeOpacity={1} onPress={positionRoute}>
+            <LayoutCustom horizontal itemsCenter justify='flex-start' mv={theme.margins.medium} pl={theme.paddings.large}>
+              <Image style={themedStyles.img} source={require("../../../assets/Icons/moneyStat.png")} />
+              <LayoutCustom ml={theme.margins.small} style={{ alignItems: "flex-start" }}>
+                <Text style={themedStyles.position}>Posiciones</Text>
+                <Text style={themedStyles.moneyText}>{isLoading ? <ActivityIndicator size={'small'} /> : currencyFormat(currency === "ARS" ? positions.arsPositions : positions.usdPositions, currency)}</Text>
+              </LayoutCustom>
+            </LayoutCustom>
+          </TouchableOpacity>
+          <LayoutCustom
+            horizontal
+            itemsCenter
+            justify="center"
+          >
+            <LayoutCustom alignSelfCenter style={themedStyles.buttonContainer}>
+              <IButton onPress={() => selectOrder(orderOptions.EMIT)} name={require(`../../../assets/Icons/ordenIcon.png`)} icon="wallet_send" title={`Informar\norden`} />
+            </LayoutCustom>
+            <LayoutCustom style={themedStyles.buttonContainer}>
+              <IButton onPress={() => selectOrder(orderOptions.REQUEST)} name={require(`../../../assets/Icons/transferIcon.png`)} icon="document" title={`Solicitar\ntransferencia`} />
             </LayoutCustom>
           </LayoutCustom>
-        </TouchableOpacity>
-        <LayoutCustom
-          horizontal
-          itemsCenter
-          justify="center"
-        >
-          <LayoutCustom alignSelfCenter style={themedStyles.buttonContainer}>
-            <IButton onPress={() => selectOrder(orderOptions.EMIT)} name={require(`../../../assets/Icons/ordenIcon.png`)} icon="wallet_send" title={`Informar\norden`} />
-          </LayoutCustom>
-          <LayoutCustom style={themedStyles.buttonContainer}>
-            <IButton onPress={() => selectOrder(orderOptions.REQUEST)} name={require(`../../../assets/Icons/transferIcon.png`)} icon="document" title={`Solicitar\ntransferencia`} />
-          </LayoutCustom>
         </LayoutCustom>
-      </LayoutCustom>
-    </Container>
+      </Container>
+    </>
   )
 }
 
