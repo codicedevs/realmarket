7fabd299233eb1973687f7a6bd2c9a19b49f95e2
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-06-26 17:09:42 -0300

    baseurl for client

--- admin/src/utils/config.ts
+++ admin/src/utils/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://192.168.1.5:3011"
-// export const BASE_URL = "https://codice.dev:3011";
+// export const BASE_URL = "http://192.168.1.5:3011"
+export const BASE_URL = "https://codice.dev:3011";
