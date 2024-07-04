bdad39429370ca6d7329e1623d94a8a8aa8f4b94
dorefici <orefici.diego@gmail.com>
2024-07-04 14:49:20 -0300

    fix button for ios and upload ios app.json

--- mobile/components/Buttons/ConfigButton.tsx
+++ mobile/components/Buttons/ConfigButton.tsx
@@ -6,30 +6,31 @@ const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 const ConfigButton = ({ title, icon, onPress }) => {
-    return (
-        <TouchableOpacity onPress={() => onPress()} style={themedStyles.buttonContainer}>
-            <Image resizeMode='center' style={{ height: 'auto', width: windowHeight * 0.03 }} source={icon} />
-            <Text style={themedStyles.buttonText}>{title}</Text>
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity onPress={() => onPress()} style={themedStyles.buttonContainer}>
+      <Image resizeMode='contain' style={{ height: 30, width: 30 }} source={icon} />
+      <Text style={themedStyles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  )
 }
 
 export default ConfigButton
 
 const themedStyles = StyleService.create({
-    buttonContainer: {
-        backgroundColor: "#009F9F",
-        marginVertical: 2,
-        paddingHorizontal: theme.paddings.medium,
-        paddingVertical: theme.paddings.small,
-        borderRadius: theme.borderRadius.medium,
-        flexDirection: 'row',
-        height: windowHeight * 0.06
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: theme.fontSizes.caption,
-        marginLeft: theme.margins.small,
-        fontFamily: 'Lato-Regular'
-    }
+  buttonContainer: {
+    backgroundColor: "#009F9F",
+    marginVertical: 2,
+    paddingHorizontal: theme.paddings.medium,
+    paddingVertical: theme.paddings.small,
+    borderRadius: theme.borderRadius.medium,
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: windowHeight * 0.06
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: theme.fontSizes.caption,
+    marginLeft: theme.margins.small,
+    fontFamily: 'Lato-Regular'
+  }
 });
