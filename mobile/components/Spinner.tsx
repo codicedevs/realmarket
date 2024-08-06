8a6f28b369468934c38b8719d5f0781cf01cc3a3
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-08-06 17:23:03 -0300

    loader and spinner

--- mobile/components/Spinner.tsx
+++ mobile/components/Spinner.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import LoaderModal from './LoaderModal';
+import { ActivityIndicator, View } from 'react-native';
+import theme from '../utils/theme';
 
 const Spinner = ({ show }) => {
   if (!show) return null
 
   return (
-    <LoaderModal />
+    <View style={{ position: 'absolute', backgroundColor: 'rgba(0,0,0, 0.5)', justifyContent: 'center', alignContent: 'center', zIndex: 100, height: '100%', width: '100%' }}>
+      <ActivityIndicator size={"large"} color={theme.colors.white} />
+    </View>
   )
 }
 
--- mobile/context/LoadingProvider.tsx
+++ mobile/context/LoadingProvider.tsx
@@ -1,11 +1,14 @@
 import React, { createContext, useContext, useState } from "react";
+import LoaderModal from "../components/LoaderModal";
 import Spinner from "../components/Spinner";
 
 type LoadingContextType = {
   loadingScreen: boolean;
   setLoadingScreen: (loadingScreen: boolean) => void;
   isLoading: boolean;
-  setIsLoading: (isLoading: boolean) => void
+  setIsLoading: (isLoading: boolean) => void;
+  loadingModal: boolean;
+  setLoadingModal: (isLoading: boolean) => void
 };
 
 const LoadingContext = createContext<LoadingContextType>({
@@ -13,16 +16,20 @@ const LoadingContext = createContext<LoadingContextType>({
   setLoadingScreen: () => { },
   isLoading: false,
   setIsLoading: () => { },
+  loadingModal: false,
+  setLoadingModal: () => { }
 });
 
 export function LoadingProvider(props: React.PropsWithChildren) {
   const [loadingScreen, setLoadingScreen] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [loadingModal, setLoadingModal] = useState<boolean>(false)
 
   return (
     <LoadingContext.Provider
-      value={{ loadingScreen, setLoadingScreen, isLoading, setIsLoading }}
+      value={{ loadingScreen, setLoadingScreen, isLoading, setIsLoading, loadingModal, setLoadingModal }}
     >
+      <LoaderModal show={loadingModal} />
       <Spinner show={loadingScreen} />
       {props.children}
     </LoadingContext.Provider>
