0cac6c68fb92a72a0c5215d515af4976255d8f0e
Ramiro Trovant <ramiro.trovant@prowidesoftware.com>
2024-08-06 17:25:31 -0300

    fixing infoProvider, missing clean

--- mobile/context/InfoProvider.tsx
+++ mobile/context/InfoProvider.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useQuery, useRealm } from "@realm/react";
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { BSON } from "realm";
 import { ContainerArs, ContainerUsd, Position } from "../Realm/Schemas";
 import disponibilidadService from "../service/disponibilidad.service";
@@ -8,19 +8,15 @@ import movimientosService from "../service/movimientos.service";
 import { CifrasDisponibilidad, currencyMovements, currencyPositions } from "../types/financial.types";
 import { useLoading } from "./LoadingProvider";
 
-
 type InfoContextType = {
-  // esto es para el home
   cifrasDisponibilidad: CifrasDisponibilidad;
   setCifrasDisponibilidad: (cifrasDisponibilidad: CifrasDisponibilidad) => void;
-  //esto es para la pantalla positions
   currencyPositions: currencyPositions;
-  setCurrencyPositions: (currencyPositions: currencyPositions) => void
-  // esto es para la pantalla de disponibilidad 
+  setCurrencyPositions: (currencyPositions: currencyPositions) => void;
   movements: currencyMovements;
   setMovements: (movements: currencyMovements) => void;
-  getUserData: () => void;
   getAllData: () => void;
+  clearAllDataFromRealm: () => void;
 };
 
 const InfoContext = createContext<InfoContextType>({
@@ -30,66 +26,37 @@ const InfoContext = createContext<InfoContextType>({
   setCurrencyPositions: () => { },
   movements: null,
   setMovements: () => { },
-  getUserData: () => { },
   getAllData: () => { },
+  clearAllDataFromRealm: () => { },
 });
 
 export function InfoProvider(props: React.PropsWithChildren) {
-  const [cifrasDisponibilidad, setCifrasDisponibilidad] = useState<CifrasDisponibilidad>(null)
-  const [currencyPositions, setCurrencyPositions] = useState<currencyPositions>(null)
-  const [movements, setMovements] = useState<currencyMovements>(null)
-  const { setIsLoading, setLoadingScreen } = useLoading()
-
+  const [cifrasDisponibilidad, setCifrasDisponibilidad] = useState<CifrasDisponibilidad>(null);
+  const [currencyPositions, setCurrencyPositions] = useState<currencyPositions>(null);
+  const [movements, setMovements] = useState<currencyMovements>(null);
+  const { setIsLoading, setLoadingModal } = useLoading();
 
   const realm = useRealm();
-  const info3 = useQuery(ContainerUsd)
-  const info2 = useQuery(ContainerArs)
-  const info1 = useQuery(Position)
-  // let Usd = info3 ? useObject(ContainerUsd, info3[0]?.id) : undefined;
-  // let Ars = info2 ? useObject(ContainerArs, info2[0]?.id) : undefined;
-  // let position = info1 ? useObject(Position, info1[0]?._id) : undefined;
-  let Usd = undefined;
-  let Ars = undefined;
-  let position = undefined;
-  const getUserData = async () => {
+  const info3 = useQuery(ContainerUsd);
+  const info2 = useQuery(ContainerArs);
+  const info1 = useQuery(Position);
+
+  let Usd = info3.length ? info3[0]?.id : undefined;
+  let Ars = info2.length ? info2[0]?.id : undefined;
+  let position = info1.length ? info1[0]?._id : undefined;
+
+  const clearAllDataFromRealm = async () => {
     try {
-      setLoadingScreen(true)
-      setIsLoading(true)
-      // primera parte donde RECIBO datos 1.
-      const res = await disponibilidadService.getCashPositions() // cifras disponibilidad de home
-      const resPos = await disponibilidadService.totalPositions()// datos de las positions de pantalla posiciones
-      const [resArs, resUsd] = await Promise.all([
-        movimientosService.getMovementsArs(),
-        movimientosService.getMovementsUsd()
-      ])
-      //
-      //aca voy a SETO todo 2.
-      setCifrasDisponibilidad(res.data)
-      setCurrencyPositions({
-        arsPositions: resPos.data.totalPosiciones,
-        usdPrice: resPos.data.usdPrice,
-        usdPriceBcra: resPos.data.usdPriceBcra,
-        posiciones: resPos.data.posiciones
-      })
-      setMovements({
-        movementsArs: resArs.data.reverse(),
-        movementsUsd: resUsd.data.reverse()
-      })
-      //
-    } catch (e) {
-
-    } finally {
-      setLoadingScreen(false)
-      setIsLoading(false)
+      realm.write(() => {
+        realm.deleteAll();
+      });
+      console.log('All data cleared from Realm.');
+    } catch (error) {
+      console.error('Error clearing data from Realm:', error);
     }
-  }
-
-  const guardatodoenrealm = () => {
-
-  }
+  };
 
   const safeRealmWrite = async (action) => {
-    //esto lo tengo que usar para las creaciones finales
     try {
       realm.write(() => {
         action();
@@ -100,39 +67,31 @@ export function InfoProvider(props: React.PropsWithChildren) {
   };
 
   const settingData = async () => {
-    // aca seteo lo que ya esta cargado
-    const totalPositions = await AsyncStorage.getItem('totalPositions')
-    const positionsByDate = await AsyncStorage.getItem('positionsByDate')
+    const positionsByDate = await AsyncStorage.getItem('positionsByDate');
     if (positionsByDate) {
-      // setCifrasDisponibilidad(JSON.parse(positionsByDate))
+      setCifrasDisponibilidad(JSON.parse(positionsByDate));
     }
-    if (totalPositions) {
-      // setPositions(Number(totalPositions))
-    }
-  }
+  };
 
   const getAllData = async () => {
+    if (!info1[0] || !info2[0] || !info3[0]) setLoadingModal(true);
     try {
-      // setLoading(true);
-      //Busco todos los datos
       const [res, resPos, resArs, resUsd] = await Promise.all([
         disponibilidadService.getCashPositions(),
         disponibilidadService.totalPositions(),
         movimientosService.getMovementsArs(),
         movimientosService.getMovementsUsd()
       ]);
-      //Manejo todos los datos con estas funciones
-      handlePositionsData(res, resPos);
-      handleMovementsData(resArs, 'ContainerArs');
-      handleMovementsData(resUsd, 'ContainerUsd');
+      await handlePositionsData(res, resPos);
+      await handleMovementsData(resArs, 'ContainerArs');
+      await handleMovementsData(resUsd, 'ContainerUsd');
     } catch (err) {
       console.error('Error fetching data:', err);
     } finally {
-      // setLoading(false);
+      setLoadingModal(false);
     }
-  }
+  };
 
-  // aca guardo lo de movimientos en dolares y pesos
   const handleMovementsData = async (res, containerType) => {
     const Container = containerType === 'ContainerUsd' ? Usd : Ars;
 
@@ -153,17 +112,13 @@ export function InfoProvider(props: React.PropsWithChildren) {
     }
   };
 
-  //y aca guardo la disponibilidad y posicones
   const handlePositionsData = async (res, resPos) => {
     if (Object.keys(res.data).length && Object.keys(resPos.data).length) {
       setCifrasDisponibilidad(res.data);
-      // setPositions(resPos.data.totalPosiciones);
       const positionsData = JSON.stringify(resPos.data.totalPosiciones);
       const positionByDate = JSON.stringify(res.data);
       await AsyncStorage.setItem('positionsByDate', positionByDate);
       await AsyncStorage.setItem('totalPositions', positionsData);
-      console.log(resPos.data, 'llega mal?')
-      // console.log('Checking if position exists...');
       await safeRealmWrite(() => {
         if (position) {
           position.totalPosiciones = resPos.data.totalPosiciones;
@@ -183,13 +138,26 @@ export function InfoProvider(props: React.PropsWithChildren) {
     }
   };
 
+  useEffect(() => {
+    settingData();
+  }, []);
+
   return (
     <InfoContext.Provider
-      value={{ cifrasDisponibilidad, currencyPositions, setCurrencyPositions, setCifrasDisponibilidad, movements, setMovements, getUserData, getAllData }}
+      value={{
+        cifrasDisponibilidad,
+        currencyPositions,
+        setCurrencyPositions,
+        setCifrasDisponibilidad,
+        movements,
+        setMovements,
+        getAllData,
+        clearAllDataFromRealm
+      }}
     >
       {props.children}
     </InfoContext.Provider>
-  )
+  );
 }
 
-export const useInfo = () => useContext(InfoContext)
+export const useInfo = () => useContext(InfoContext);
